f2d4af0bae0ff7ec01f7ac9461092fd7909f6cf2
Yoshiki Kobayashi <attack_no22001@yahoo.co.jp>
2016-03-25 17:52:51 +0900

    レウアウトファイル用意 ajax用のルーティングも用意

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require 'sinatra/reloader'
 require 'slim'
 require 'sass'
 require 'coffee-script'
+require 'logger'
+
+$stdout.sync = true
+logger = Logger.new('sinatra.log')
 
 class App < Sinatra::Base
   configure :development do
@@ -12,5 +16,23 @@ class App < Sinatra::Base
   # routingの設定
   get '/' do
     slim :index
+  end
+
+  get '/scroll' do
+    slim :scroll 
+  end
+
+  get '/kizashi' do
+    require 'rexml/document'
+    require 'net/http'
+
+    #url = "http://b.hatena.ne.jp/entry/json/?url=" + URI.escape("http://anond.hatelabo.jp/20160215171759")
+    url = "http://b.hatena.ne.jp/entry/json/?url=" + CGI.escape("http://anond.hatelabo.jp/20160215171759")
+    uri = URI.parse(url)
+
+    @xml = Net::HTTP.get(uri)
+    @xml = @xml.encode("UTF-8")
+
+    slim :kizashi, :layout=>:ajax_layout
   end
 end
